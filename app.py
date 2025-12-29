33c3b4a0e9781c794a6f265b3b6229c1aa79a8b1
ghatageakshay <ghatageakshay96@gmail.com>
2025-12-29 11:41:03 +0530

    add login/signup flow,profile page,fix sqllite issues

--- app.py
+++ app.py
@@ -23,28 +23,41 @@ def signup():
 
         conn=get_db()
         cursor=conn.cursor()
-        cursor.execute(
-            "INSERT INTO users(name,email,password,skill_level,interests) values(?,?,?,?,?)", 
-            (name,email,password,skill,interests)
+        try:
+            cursor.execute(
+                "INSERT INTO users(name,email,password,skill_level,interests) values(?,?,?,?,?)", 
+                (name,email,password,skill,interests)
             
         )
 
-        conn.commit()
-        conn.close()
+            conn.commit()
+            user_id=cursor.lastrowid
+            
 
-        return redirect("/profile")
+            session["user_id"]=user_id
+            session["user_name"]=name
+
+            return redirect("/dashboard")
+        
+        except sqlite3.IntegrityError:
+            return render_template("signup.html",error="Email Alreday registred")
     
+        finally:
+            conn.close()
+
     return render_template("signup.html")
+        
 
-@app.route("/profile")
-def profile():
-    conn=get_db()
-    cursor=conn.cursor()
-    cursor.execute("SELECT * FROM users ORDER BY id DESC LIMIT 1")
-    user=cursor.fetchone()
-    conn.close()
 
-    return f"WELCOME {user[1]}! Skill:{user[4]},Interests: {user[5]}"
+# @app.route("/profile")
+# def profile():
+#     conn=get_db()
+#     cursor=conn.cursor()
+#     cursor.execute("SELECT * FROM users ORDER BY id DESC LIMIT 1")
+#     user=cursor.fetchone()
+#     conn.close()
+
+#     return f"WELCOME {user[1]}! Skill:{user[4]},Interests: {user[5]}"
 
 
 @app.route("/")
@@ -85,5 +98,51 @@ def logout():
     session.clear()
     return redirect("/login")
 
+@app.route("/profile")
+def profile():
+    if "user_id" not in session:
+        return redirect("/login")
+    
+    user_id=session["user_id"]
+    conn=get_db()
+    cursor=conn.cursor()
+    cursor.execute("select name,email,skill_level,interests FROM users WHERE id=?",(user_id,))
+    user=cursor.fetchone()
+    
+    return render_template("profile.html",user=user)
+
+@app.route("/profile/edit",methods=["GET","POST"])
+def edit_profile():
+    if "user_id" not in session:
+        return redirect("/login")
+    
+    conn=get_db()
+    cursor=conn.cursor()
+
+    if request.method=="POST":
+        name=request.form["name"]
+        skill=request.form["skill_level"]
+        interests=request.form["interests"]
+
+        cursor.execute("""
+                       UPDATE users
+                       set name=?,skill_level=?,interests=?
+                       where id=?
+                       """,
+                       (name,skill,interests,session["user_id"])
+
+        )
+
+        conn.commit()
+        conn.close()
+
+        session["user_name"]=name 
+        return redirect("/profile")
+    
+    cursor.execute("SELECT name,skill_level,interests from users where id=?",(session["user_id"],))
+    user=cursor.fetchone()
+    conn.close()
+
+    return render_template("edit_profile.html",user=user)
 if __name__=="__main__":
     app.run(debug=True)
